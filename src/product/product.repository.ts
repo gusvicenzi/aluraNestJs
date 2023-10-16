b92b821ce6e152a72fbd9dd82b5b0eb172ca3333
Gustavo Vicenzi <gusvicenzi12@gmail.com>
2023-10-16 16:43:42 -0300

    Add product module

---
+++ src/product/product.repository.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common'
+
+@Injectable()
+export class ProductRepository {
+  private products = []
+
+  async save(product) {
+    this.products.push(product)
+    console.log(this.products)
+  }
+
+  async list() {
+    return this.products
+  }
+}
