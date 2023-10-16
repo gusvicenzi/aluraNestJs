03028350240764b46ec47e96789808755ea5beed
Gustavo Vicenzi <gusvicenzi12@gmail.com>
2023-10-16 15:28:04 -0300

    Upload files

---
+++ src/user.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, Get, Post } from '@nestjs/common'
+import { UserRepository } from './user.repository'
+
+@Controller('/users')
+export class UserController {
+  private userRepository = new UserRepository()
+
+  @Post()
+  async createUser(@Body() userData) {
+    this.userRepository.save(userData)
+    return { status: 'usuario criado!', userData }
+  }
+
+  @Get()
+  async listUsers() {
+    return this.userRepository.list()
+  }
+}
---
+++ src/user.repository.ts
@@ -0,0 +1,12 @@
+export class UserRepository {
+  private users = []
+
+  async save(user) {
+    this.users.push(user)
+    console.log(this.users)
+  }
+
+  async list() {
+    return this.users
+  }
+}
