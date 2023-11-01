ca2d9ee0515841592d6a0038e0f6cd189cb4101a
Gustavo Vicenzi <gusvicenzi12@gmail.com>
2023-11-01 11:46:19 -0300

    Add bcrypt hashing password

--- src/modules/user/dto/CreateUser.dto.ts
+++ src/modules/user/dto/CreateUser.dto.ts
@@ -1,4 +1,11 @@
-import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator'
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength
+} from 'class-validator'
 import { IsEmailUnique } from '../validation/isEmailUnique.validator'
 
 export class CreateUserDTO {
@@ -10,6 +17,11 @@ export class CreateUserDTO {
   @IsEmailUnique({ message: 'E-mail já cadastrado.' })
   email: string
 
-  @MinLength(6, { message: 'A senha precisa ter ao menos 6 caracteres.' })
+  @MinLength(8, { message: 'A senha precisa ter ao menos 8 caracteres.' })
+  @MaxLength(30, { message: 'A senha precisa ter no máximmo 30 caracteres.' })
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W+).{6,30}$/, {
+    message:
+      'A senha deve possuir ao menos 1 letra minúscula, 1 letra maiúscula, 1 dígito numérico e um caractere especial.'
+  })
   senha: string
 }
--- src/resources/pipes/hashing-password.pipe.ts
+++ src/resources/pipes/hashing-password.pipe.ts
@@ -1,8 +1,15 @@
 import { Injectable, PipeTransform } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import * as bcrypt from 'bcrypt'
 
 @Injectable()
 export class HashingPasswordPipe implements PipeTransform {
-  transform(senha: string) {
-    return senha + 'abcdef'
+  constructor(private configService: ConfigService) {}
+  async transform(senha: string) {
+    const salt = this.configService.get<string>('SALT_PASSWORD')
+
+    const hashedSenha = await bcrypt.hash(senha, salt!)
+
+    return hashedSenha
   }
 }
