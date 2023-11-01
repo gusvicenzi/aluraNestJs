422a10782f5564d6b6c5598ac1bb01044f96b7f0
Gustavo Vicenzi <gusvicenzi12@gmail.com>
2023-11-01 17:29:50 -0300

    Move jwt secret to .end

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,16 +3,20 @@ import { AuthService } from './auth.service'
 import { AuthController } from './auth.controller'
 import { UserModule } from '../user/user.module'
 import { JwtModule } from '@nestjs/jwt'
+import { ConfigService } from '@nestjs/config'
 
 @Module({
   imports: [
     UserModule,
-    JwtModule.register({
-      secret: 'SEGREDO',
-      global: true,
-      signOptions: {
-        expiresIn: '72h'
-      }
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWS_SECRET'),
+        signOptions: {
+          expiresIn: '72h'
+        }
+      }),
+      inject: [ConfigService],
+      global: true
     })
   ],
   controllers: [AuthController],
